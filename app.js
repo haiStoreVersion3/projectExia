195f23e0048d2150918aa7ef46c0ce741bb1452b
Richard Hai <hirichardhai@gmail.com>
2018-07-31 22:15:54 -0700

    added event listener, added get request ajax post link, setup parse information

--- app.js
+++ app.js
@@ -51,3 +51,36 @@ $(window).load(function(){
         scrollFooter(scroll, footerHeight);
     }
 });
+
+app = {
+  init: () => {
+      // app.fetch();
+      $('#submitButton').on('submit', (e) => {
+          e.preventDefault();
+          app.send($('#input').val())
+          // console.log($('#input').val())
+      })
+  },
+  send: (data) => {
+      $.ajax({
+          url: '/',
+          type: 'POST',
+          contentType: 'application/json',
+          crossDomain: true,
+          data: data,
+          success: (returned) => {
+              console.log('success, sent!');
+              app.parseResponse(returned);
+          },
+          error: (data) => {
+              console.error('failed to send');
+          }
+      })
+  },
+  parseResponse: (data) => {
+          var arr = data.split('\n');
+      arr.forEach((line) => {
+          $('#convertedCSV').append(`<div>${line}</div>`);
+      })
+  }
+}
