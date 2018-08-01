462dd9661ee3b40bc158520cf457a4f65a938dbc
Richard Hai <hirichardhai@gmail.com>
2018-07-31 21:20:54 -0700

    successfully serving index html

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('../index.html'));
+app.use("/", express.static(__dirname));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
