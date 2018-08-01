d1940a9bd47dffd1a31421e3a6419cb5df81b7a0
Richard Hai <hirichardhai@gmail.com>
2018-07-31 21:05:49 -0700

    added express server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
