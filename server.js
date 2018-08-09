16c5b17f9f0475ecfd17bcde9fb7304e7d16d63e
Richard Hai <hirichardhai@gmail.com>
2018-08-08 23:40:08 -0700

    added back a post request towards exia route

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.use("/", static(__dirname));
 
-// app.post('/exia', function (req, res) {
-//   var username = req.body
-//   res.send(`you just saved ${username} to the database`);
-// });
+app.post('/exia', function (req, res) {
+  var username = req.body
+  res.send(`you just saved ${username} to the database`);
+});
 
 app.listen(3000, () => console.log('Server for project exia launched!!!'))
